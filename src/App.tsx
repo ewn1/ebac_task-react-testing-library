fcc2b30dc1a7bb2c8d256c675186680689f3ae94
Edwin <ewn.francisco@gmail.com>
2026-04-19 20:31:03 -0300

    feat: integrando testes com jest

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import Post from "./components/Post";
 
-import styles from './App.module.css';
+import styles from "./App.module.css";
 
 function App() {
   return (
     <div className={styles.app}>
-      <Post imageUrl="https://www.orangeboxminiaturas.com.br/img/products/batmovel-1989-figura-batman-em-metal-jada-toys-1-24-jad-98260_1_1000.jpg">
+      <Post imageUrl="https://www.minimundi.com.br/cdn/imagens/produtos/det/miniatura-carro-batmovel-1989-c-figura-metals-die-cast-1-32-jada-toys_a.jpg">
         Olha só que legal minha miniatura do Batmóvel.
       </Post>
     </div>
--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -1,10 +1,25 @@
-import { fireEvent, render, screen } from '@testing-library/react';
-import Post from '.';
-import PostComment from '.';
+import { fireEvent, render, screen } from "@testing-library/react";
+import Post from ".";
 
-describe('Teste para o componente PostComment', () => {
-    it('Deve renderizar o componente corretamente', () => {
-        render(<PostComment/>);
-        expect(screen.getByText('Comentar')).toBeInTheDocument();
+describe("Teste para o componente Post", () => {
+  test("Deve renderizar dois comentários corretamente", () => {
+    render(<Post />);
+    const commentInput = screen.getByTestId("comment-input");
+    const commentButton = screen.getByTestId("comment-button");
+
+    fireEvent.change(commentInput, {
+      target: { value: "Primeiro comentário do teste" },
     });
+    fireEvent.click(commentButton);
+
+    fireEvent.change(commentInput, {
+      target: { value: "Segundo comentário do teste" },
+    });
+    fireEvent.click(commentButton);
+
+    const comments = screen.getAllByTestId("comment");
+    expect(comments).toHaveLength(2);
+    expect(comments[0].innerHTML).toContain("Primeiro comentário do teste");
+    expect(comments[1].innerHTML).toContain("Segundo comentário do teste");
+  });
 });
